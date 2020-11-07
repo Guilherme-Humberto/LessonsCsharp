8bd4fdcf8259352c49d528d6f66ab86c03254fb6
Guilherme-Humberto <gbreu@outlook.com>
2020-11-07 13:37:27 -0300

    Funções de tipos string

---
+++ lesson3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite alguma coisa");
+            string coisa = Console.ReadLine();
+
+            Console.WriteLine("Maiúsculo: " + coisa.ToUpper());
+            Console.WriteLine("Minúsculo: " + coisa.ToLower());
+            Console.WriteLine("Tamanho: " + coisa.Length + " letras");
+            Console.WriteLine("Parte do valor: " + coisa.Substring(0, 3));
+            Console.WriteLine("Contém: " + coisa.Contains("Gui"));
+        }
+    }
+}

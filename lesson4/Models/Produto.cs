cdd7c26b77fced57fa3d35f7b313ea5c80097e36
Guilherme-Humberto <gbreu@outlook.com>
2020-11-07 13:58:45 -0300

    Alterando propriedades de entidades

---
+++ lesson4/Models/Produto.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace lesson4.Models
+{
+    public class Produto
+    {
+        // Propriedades
+        public string Nome { get; set; }
+        public int Preco { get; set; }
+
+        public void Produtos(string nome, int valor)
+        {
+            this.Nome = nome;
+            this.Preco = valor;
+        }
+
+        public string compareValue(int valor) {
+            if(valor < this.Preco) {
+                return "Você não pode pagar por esta " + this.Nome;
+            }
+            else {
+                return "Você deve pagar R$:" + (valor - this.Preco);
+            }
+        }
+    }
+}
---
+++ lesson4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using lesson4.Models;
+
+namespace lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int carteira = 20;
+
+            Console.WriteLine("Digite o nome do produto que deseja comprar");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o valor do produto");
+            int valor = int.Parse(Console.ReadLine());
+
+            // Criando uma instância(objeto) da classe produto
+            var produto = new Produto();
+            produto.Produtos(nome, valor);
+
+            Console.WriteLine(produto.compareValue(carteira));
+        }
+    }
+}

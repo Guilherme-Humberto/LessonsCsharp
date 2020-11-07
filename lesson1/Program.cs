661e85b8d73818d991d85debea36d891b434c729
Guilherme-Humberto <gbreu@outlook.com>
2020-11-07 13:34:59 -0300

    Pegando valores do teclado e trabalhando com classes

---
+++ lesson1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome do usuário");
+            string nome = Console.ReadLine();
+
+            User usuario = new User();
+            string nomedigitado = usuario.Users(nome);
+            
+            Console.WriteLine("O nome digitado foi " + nomedigitado);
+        }
+    }
+}
---
+++ lesson1/models/Users.cs
@@ -0,0 +1,10 @@
+class User
+{
+    public string Nome { get; set; }
+
+    public string Users(string nome)
+    {
+        this.Nome = nome;
+        return Nome;
+    }
+}

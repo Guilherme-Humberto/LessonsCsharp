cb5ad862b99f56e827eb549732f5c8143f807742
Guilherme-Humberto <gbreu@outlook.com>
2020-11-07 21:58:09 -0300

    Constantes

---
+++ lesson8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace lesson8
+{
+    class Program
+    {
+        // Ao trabalhar com variáveis constantes, tem de se atribuir algum valor
+        const string Nome = "Guilherme Humberto";
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Nome);
+            // Variáveis constantes são imutáveis;
+        }
+    }
+}

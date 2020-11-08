20b6c3742c2c6516953ebe4fb1a93ad2b16cdb3a
Guilherme-Humberto <gbreu@outlook.com>
2020-11-07 22:20:40 -0300

    Classes estáticas

--- lesson7/models/User.cs
+++ lesson7/models/User.cs
@@ -1,6 +1,7 @@
 static class User
 {
     // As propriedades e métodos de uma classe estática tem de ser estáticas.
+    // São acessíveis a outras classes do sistema.
     public static string Name { get; set; }
     public static string Surname { get; set; }
 
